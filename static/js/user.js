20824a311b8af8e576a980194f6140f5cbd4de9b
Paul Frazee <pfrazee@gmail.com>
2021-02-12 11:11:30 -0600

    Fix potential undefined accesses

--- static/js/user.js
+++ static/js/user.js
@@ -279,7 +279,7 @@ class CtznUser extends LitElement {
         <div class="max-w-4xl mx-auto grid grid-cols-layout-twocol gap-8">
           <div>
             <h3 class="text-lg mb-4 font-semibold">Following ${this.following?.length} ${pluralize(this.following?.length, 'Citizen')}</h3>
-            <ctzn-user-list .ids=${this.following.map(f => f.value.subject.userId)}></ctzn-user-list>
+            <ctzn-user-list .ids=${this.following?.map(f => f.value.subject.userId)}></ctzn-user-list>
           </div>
           ${this.renderRightSidebar()}
         </div>
@@ -289,7 +289,7 @@ class CtznUser extends LitElement {
         <div class="max-w-4xl mx-auto grid grid-cols-layout-twocol gap-8">
           <div>
             <h3 class="text-lg mb-4 font-semibold">${this.members?.length} ${pluralize(this.members?.length, 'Member')}</h3>
-            <ctzn-user-list .ids=${this.members.map(f => f.value.user.userId)}></ctzn-user-list>
+            <ctzn-user-list .ids=${this.members?.map(f => f.value.user.userId)}></ctzn-user-list>
           </div>
           ${this.renderRightSidebar()}
         </div>
