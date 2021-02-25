cf898891991288183e70999ecf8ab586f35215ae
Paul Frazee <pfrazee@gmail.com>
2021-02-25 12:58:10 -0600

    Add 'suggested communities' to the sidebar

--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,24 @@ import './com/login.js'
 import './com/feed.js'
 import './com/img-fallbacks.js'
 
+const SUGGESTED_COMMUNITIES = [
+  {
+    userId: 'foo@foo.com',
+    displayName: 'Cool community',
+    description: 'The coolest community in CTZN.'
+  },
+  {
+    userId: 'foo@foo.com',
+    displayName: 'Test data',
+    description: 'These suggestions are not real.'
+  },
+  {
+    userId: 'foo@foo.com',
+    displayName: 'Test data',
+    description: 'We\'ll choose real suggestions during Alpha Launch Saturday!'
+  }
+]
+
 class CtznApp extends LitElement {
   static get properties () {
     return {
@@ -33,6 +51,7 @@ class CtznApp extends LitElement {
     this.numNewItems = 0
     this.loadTime = Date.now()
     this.memberships = undefined
+    this.suggestedCommunities = undefined
 
     this.load()
 
@@ -52,6 +71,9 @@ class CtznApp extends LitElement {
       return this.requestUpdate()
     }
     this.memberships = await listMemberships(session.info.userId)
+    if (!this.suggestedCommunities) {
+      this.suggestedCommunities = SUGGESTED_COMMUNITIES.filter(c => !this.memberships?.find(m => c.userId === m.value.community.userId))
+    }
 
     if (this.querySelector('ctzn-feed')) {
       this.loadTime = Date.now()
@@ -87,36 +109,6 @@ class CtznApp extends LitElement {
   render () {
     return html`
       ${this.renderCurrentView()}
-    `
-  }
-
-  renderRightSidebar () {
-    return html`
-      <nav>
-        ${''/*todo <section class="mb-4">
-          <span class="fas fa-search"></span>
-          ${!!this.searchQuery ? html`
-            <a class="clear-search" @click=${this.onClickClearSearch}><span class="fas fa-times"></span></a>
-          ` : ''}
-          <input @keyup=${this.onKeyupSearch} placeholder="Search" value=${this.searchQuery}>
-        </section>*/}
-        <section class="pt-1 mb-4">
-          <h3 class="font-medium mb-1">My Communities</h3>
-          ${this.memberships?.length ? html`
-            <div class="mt-2">
-              ${repeat(this.memberships, membership => html`
-                <div>
-                  <a class="inline-block bg-gray-100 rounded py-1 px-2 mb-1 text-sm hover:bg-gray-200" href="/${membership.value.community.userId}">${displayNames.render(membership.value.community.userId)}</a>
-                </div>
-              `)}
-            </div>
-          ` : html`
-            <div class="px-3 py-2 bg-gray-100 text-gray-500 text-xs">
-              Join a community to get connected to more people!
-            </div>
-          `}
-        </section>
-      </nav>
     `
   }
 
@@ -209,10 +201,70 @@ class CtznApp extends LitElement {
       `
     }
     return html`
-      <div class="bg-gray-100 text-gray-500 py-44 text-center my-5">
+      <div class="bg-gray-100 text-gray-500 py-44 text-center my-4">
         <div class="fas fa-stream text-6xl text-gray-300 mb-8"></div>
-        <div>Subscribe to citizens<br>or join communities to see what's new.</div>
+        <div>Follow people and<br>join communities to see what's new.</div>
       </div>
+    `
+  }
+
+  renderRightSidebar () {
+    return html`
+      <nav class="pl-1">
+        ${''/*todo <section class="mb-4">
+          <span class="fas fa-search"></span>
+          ${!!this.searchQuery ? html`
+            <a class="clear-search" @click=${this.onClickClearSearch}><span class="fas fa-times"></span></a>
+          ` : ''}
+          <input @keyup=${this.onKeyupSearch} placeholder="Search" value=${this.searchQuery}>
+        </section>*/}
+        <section class="pt-1 mb-8">
+          <h3 class="font-bold mb-2 text-gray-500 text-xs">My Communities</h3>
+          ${this.memberships?.length ? html`
+            <div class="mt-2">
+              ${repeat(this.memberships, membership => html`
+                <div>
+                  <a class="text-sm hover:pointer hover:underline" href="/${membership.value.community.userId}">${displayNames.render(membership.value.community.userId)}</a>
+                </div>
+              `)}
+            </div>
+          ` : html`
+            <div>
+              Join a community to get connected to more people!
+            </div>
+          `}
+        </section>
+        <section class="pt-1 mb-4">
+          <h3 class="font-bold mb-2 text-gray-500 text-xs">Suggested Communities</h3>
+          ${repeat(this.suggestedCommunities, community => community.userId, community => {
+            const hasJoined = this.memberships?.find(m => community.userId === m.value.community.userId)
+            return html`
+              <div class="text-sm mb-3">
+                <div>
+                  <a class="text-sm hover:pointer hover:underline" href="/${community.userId}" title=${community.displayName}>
+                  ${community.displayName}
+                  </a>
+                </div>
+                <div class="text-gray-600 mb-1">${community.description}</div>
+                <div>
+                  ${hasJoined ? html`
+                    <button
+                      class="border border-blue-400 px-4 py-0.5 rounded-2xl text-blue-600 text-sm cursor-default"
+                      disabled
+                    >Joined!</button>
+                  ` : html`
+                    <button
+                      class="border border-blue-400 hover:bg-gray-100 hover:pointer px-4 py-0.5 rounded-2xl text-blue-600 text-sm"
+                      @click=${e => this.onClickJoinSuggestedCommunity(e, community)}
+                      ?disabled=${hasJoined}
+                    >${community.isJoining ? html`<span class="spinner"></span>` : 'Join'}</button>
+                  `}
+                </div>
+              </div>
+            `
+          })}
+        </section>
+      </nav>
     `
   }
 
@@ -267,6 +319,21 @@ class CtznApp extends LitElement {
       console.log(e)
       toast.create(e.toString(), 'error')
     }
+  }
+
+  async onClickJoinSuggestedCommunity (e, community) {
+    community.isJoining = true
+    this.requestUpdate()
+    try {
+      await session.api.communities.join(community.userId)
+      toast.create('Community joined')
+    } catch (e) {
+      console.log(e)
+      toast.create(e.toString(), 'error')
+    }
+    community.isJoining = false
+    this.requestUpdate()
+    this.load()
   }
 }
 
