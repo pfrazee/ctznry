3b300e3a0d28b62e6b1c73db3955bc6ab4011eaf
Paul Frazee <pfrazee@gmail.com>
2021-04-15 12:51:10 -0500

    Slightly nicer logged-out state UI

--- static/js/com/header.js
+++ static/js/com/header.js
@@ -58,6 +58,9 @@ export class Header extends LitElement {
   }
 
   render () {
+    if (!session.isActive()) {
+      return this.renderLoggedOut()
+    }
     let info = session.getSavedInfo()
     return html`
       <header>
@@ -69,132 +72,133 @@ export class Header extends LitElement {
               <span class="text-lg text-gray-500 tracking-tight">alpha</span>
             </div>
           </div>
-          ${session.hasOneSaved() ? html`
-            <div class="flex flex-col px-2">
-              <hr class="my-3 mx-3">
-              <a href="/" class=${this.getMenuNavClass('/')} @click=${this.onClickLink}>
-                <span class="fas mr-1.5 fa-fw navicon fa-home"></span>
-                Home
-              </a>
-              <a href="/notifications" class="relative ${this.getMenuNavClass('/notifications')}" @click=${this.onClickLink}>
-                ${this.unreadNotificationsCount > 0 ? html`
-                  <span class="absolute bg-blue-500 font-medium leading-none px-1.5 py-0.5 rounded-2xl text-white text-xs" style="top: 5px; left: 22px">${this.unreadNotificationsCount}</span>
-                ` : ''}
-                <span class="fas mr-1.5 fa-fw navicon fa-bell"></span>
-                Notifications
-              </a>
-              <a href="/communities" class="${this.getMenuNavClass('/communities')}" @click=${this.onClickLink}>
-                <span class="fas mr-1.5 fa-fw navicon fa-users"></span>
-                Communities
-              </a>
-              <hr class="my-3 mx-3">
-              <a
-                class="flex items-center ${this.getMenuNavClass()} mt-1"
-                href="/${info.userId}"
-                title=${info.userId}
-                @click=${this.onClickLink}
-              >
-                <span class="inline-block mr-2" style="margin-left: -3px">
-                  <img
-                    class="inline-block w-8 h-8 sm:w-7 sm:h-7 object-cover rounded"
-                    src=${AVATAR_URL(info.userId)}
-                  >
-                </span>
-                My Profile
-              </a>
-              <a href="/${info.userId}/inventory" class=${this.getMenuNavClass()} @click=${this.onClickLink}>
-                <span class="fas mr-1.5 fa-fw navicon fa-suitcase"></span>
-                My Inventory
-              </a>
-              <hr class="my-3 mx-3">
-            </div>
-            <div class="mt-3 sm:mb-auto px-4">
-              <app-button
-                primary
-                btn-class="text-base sm:text-sm font-semibold w-full mb-2 rounded-3xl"
-                label="Create Post"
-                @click=${this.onClickCreatePost}
-              ></app-button>
-              <app-button
-                btn-class="text-gray-600 text-base sm:text-sm font-semibold w-full rounded-3xl"
-                label="Create Community"
-                @click=${this.onClickCreateCommunity}
-              ></app-button>
-              <hr class="mt-5">
-            </div>
-          ` : ''}
-          <div class="py-3 px-2">
-            ${this.renderSessionCtrls()}
-          </div>
-        </div>
-        ${session.isActive() ? html`
-          <div class="secondary-menu bg-white overflow-y-auto px-2 py-2">
+          <div class="flex flex-col px-2">
+            <hr class="my-3 mx-3">
+            <a href="/" class=${this.getMenuNavClass('/')} @click=${this.onClickLink}>
+              <span class="fas mr-1.5 fa-fw navicon fa-home"></span>
+              Home
+            </a>
+            <a href="/notifications" class="relative ${this.getMenuNavClass('/notifications')}" @click=${this.onClickLink}>
+              ${this.unreadNotificationsCount > 0 ? html`
+                <span class="absolute bg-blue-500 font-medium leading-none px-1.5 py-0.5 rounded-2xl text-white text-xs" style="top: 5px; left: 22px">${this.unreadNotificationsCount}</span>
+              ` : ''}
+              <span class="fas mr-1.5 fa-fw navicon fa-bell"></span>
+              Notifications
+            </a>
+            <a href="/communities" class="${this.getMenuNavClass('/communities')}" @click=${this.onClickLink}>
+              <span class="fas mr-1.5 fa-fw navicon fa-users"></span>
+              Communities
+            </a>
+            <hr class="my-3 mx-3">
             <a
-              class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
+              class="flex items-center ${this.getMenuNavClass()} mt-1"
               href="/${info.userId}"
               title=${info.userId}
+              @click=${this.onClickLink}
             >
-              <img class="w-8 h-8 object-cover rounded-md mr-2" src=${AVATAR_URL(info.userId)} style="left: 10px; top: 6px">
-              ${displayNames.render(info.userId)}
+              <span class="inline-block mr-2" style="margin-left: -3px">
+                <img
+                  class="inline-block w-8 h-8 sm:w-7 sm:h-7 object-cover rounded"
+                  src=${AVATAR_URL(info.userId)}
+                >
+              </span>
+              My Profile
             </a>
-            <h3 class="font-bold pl-2 mt-4 mb-2 text-gray-500 text-xs">
-              My Communities
-            </h3>
-            <div class="mb-4">
-              ${session.myCommunities?.length ? html`
-                ${repeat(session.myCommunities, community => html`
-                  <a
-                    class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
-                    href="/${community.userId}"
-                  >
-                    <img
-                      class="lazyload w-8 h-8 object-cover rounded-md mr-2"
-                      data-src=${AVATAR_URL(community.userId)}
-                    >
-                    <span class="truncate font-medium">${displayNames.render(community.userId)}</span>
-                  </a>
-                `)}
-              ` : html`
-                <div class="pl-2 pr-5 mb-1 text-base text-gray-700">
-                  Join a community to get connected to more people!
-                </div>
-              `}
-              <a
-                class="flex items-center pl-2 pr-4 py-1 text-sm rounded cursor-pointer hov:hover:bg-gray-100"
-                @click=${this.onClickCreateCommunity}
-              >
-                <span
-                  class="w-8 py-1.5 text-center object-cover rounded-md mr-2 bg-gray-300 text-gray-600"
-                ><span class="fas fa-plus"></span></span>
-                <span class="truncate font-semibold text-gray-600">Create community</span>
-              </a>
-              <a
-                class="flex items-center pl-2 pr-4 py-1 text-sm rounded cursor-pointer hov:hover:bg-gray-100"
-                href="/communities"
-              >
-                <span
-                  class="w-8 py-1.5 text-center object-cover rounded-md mr-2 bg-gray-300 text-gray-600"
-                ><span class="fas fa-users"></span></span>
-                <span class="truncate font-semibold text-gray-600">Browse</span>
+            <a href="/${info.userId}/inventory" class=${this.getMenuNavClass()} @click=${this.onClickLink}>
+              <span class="fas mr-1.5 fa-fw navicon fa-suitcase"></span>
+              My Inventory
+            </a>
+            <hr class="my-3 mx-3">
+          </div>
+          <div class="mt-3 sm:mb-auto px-4">
+            <app-button
+              primary
+              btn-class="text-base sm:text-sm font-semibold w-full mb-2 rounded-3xl"
+              label="Create Post"
+              @click=${this.onClickCreatePost}
+            ></app-button>
+            <app-button
+              btn-class="text-gray-600 text-base sm:text-sm font-semibold w-full rounded-3xl"
+              label="Create Community"
+              @click=${this.onClickCreateCommunity}
+            ></app-button>
+            <hr class="mt-5">
+          </div>
+          <div class="py-3 px-2">
+            <div class="pb-16 sm:pb-6 flex flex-col">
+              <a class=${this.getMenuNavClass('/account')} href="/account" @click=${this.onClickLink}><span class="fas fa-fw fa-cog mr-1.5"></span> Account</a>
+              <a class=${this.getMenuNavClass()} href="#" @click=${this.onLogOut}>
+                <span class="fas fa-fw fa-sign-out-alt mr-1.5"></span> Log out
               </a>
             </div>
-            <h3 class="font-bold pl-2 mb-2 text-gray-500 text-xs">
-              My Follows
-            </h3>
-            ${repeat(session.myFollowing || [], f => f, f => html`
-              <a
-                class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
-                href="/${f}"
-              >
-                <img
-                  class="lazyload w-8 h-8 object-cover rounded-md mr-2"
-                  data-src=${AVATAR_URL(f)}
-                >
-                <span class="truncate font-medium">${displayNames.render(f)}</span>
-              </a>
-            `)}
           </div>
-        ` : ''}
+        </div>
+        <div class="secondary-menu bg-white overflow-y-auto px-2 py-2">
+          <a
+            class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
+            href="/${info.userId}"
+            title=${info.userId}
+          >
+            <img class="w-8 h-8 object-cover rounded-md mr-2" src=${AVATAR_URL(info.userId)} style="left: 10px; top: 6px">
+            ${displayNames.render(info.userId)}
+          </a>
+          <h3 class="font-bold pl-2 mt-4 mb-2 text-gray-500 text-xs">
+            My Communities
+          </h3>
+          <div class="mb-4">
+            ${session.myCommunities?.length ? html`
+              ${repeat(session.myCommunities, community => html`
+                <a
+                  class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
+                  href="/${community.userId}"
+                >
+                  <img
+                    class="lazyload w-8 h-8 object-cover rounded-md mr-2"
+                    data-src=${AVATAR_URL(community.userId)}
+                  >
+                  <span class="truncate font-medium">${displayNames.render(community.userId)}</span>
+                </a>
+              `)}
+            ` : html`
+              <div class="pl-2 pr-5 mb-1 text-base text-gray-700">
+                Join a community to get connected to more people!
+              </div>
+            `}
+            <a
+              class="flex items-center pl-2 pr-4 py-1 text-sm rounded cursor-pointer hov:hover:bg-gray-100"
+              @click=${this.onClickCreateCommunity}
+            >
+              <span
+                class="w-8 py-1.5 text-center object-cover rounded-md mr-2 bg-gray-300 text-gray-600"
+              ><span class="fas fa-plus"></span></span>
+              <span class="truncate font-semibold text-gray-600">Create community</span>
+            </a>
+            <a
+              class="flex items-center pl-2 pr-4 py-1 text-sm rounded cursor-pointer hov:hover:bg-gray-100"
+              href="/communities"
+            >
+              <span
+                class="w-8 py-1.5 text-center object-cover rounded-md mr-2 bg-gray-300 text-gray-600"
+              ><span class="fas fa-users"></span></span>
+              <span class="truncate font-semibold text-gray-600">Browse</span>
+            </a>
+          </div>
+          <h3 class="font-bold pl-2 mb-2 text-gray-500 text-xs">
+            My Follows
+          </h3>
+          ${repeat(session.myFollowing || [], f => f, f => html`
+            <a
+              class="flex items-center pl-2 pr-4 py-1 text-sm rounded hov:hover:bg-gray-100"
+              href="/${f}"
+            >
+              <img
+                class="lazyload w-8 h-8 object-cover rounded-md mr-2"
+                data-src=${AVATAR_URL(f)}
+              >
+              <span class="truncate font-medium">${displayNames.render(f)}</span>
+            </a>
+          `)}
+        </div>
       </header>
       ${this.isMenuOpen ? html`
         <div
@@ -205,24 +209,31 @@ export class Header extends LitElement {
     `
   }
 
-  renderSessionCtrls () {
-    if (session.hasOneSaved()) {
-      return html`
-        <div class="pb-16 sm:pb-6 flex flex-col">
-          <a class=${this.getMenuNavClass('/account')} href="/account" @click=${this.onClickLink}><span class="fas fa-fw fa-cog mr-1.5"></span> Account</a>
-          <a class=${this.getMenuNavClass()} href="#" @click=${this.onLogOut}>
-            <span class="fas fa-fw fa-sign-out-alt mr-1.5"></span> Log out
-          </a>
+  renderLoggedOut () {
+    return html`
+      <header>
+        <div class="logged-out-prompt bg-white mb-4">
+          <div class="block sm:flex pt-2 pb-4">
+            <div class="px-4 sm:px-0 flex-1">
+              <div class="font-bold text-3xl text-gray-800">
+                CTZN
+                <span class="text-lg text-gray-500 tracking-tight">alpha</span>
+              </div>
+              <div class="text-sm pb-2 text-gray-600">
+                (pronounced "Citizen")
+              </div>
+              <div class="">
+                A decentralized social network for communities.
+              </div>
+            </div>
+            <div class="">
+              <a class="inline-block px-3 py-3 font-semibold rounded hov:hover:bg-gray-100" href="/" @click=${this.onClickLink}><span class="fas fa-fw fa-sign-in-alt mr-1.5"></span> Log in</a>
+              <a class="inline-block px-3 py-3 font-semibold rounded hov:hover:bg-gray-100" href="/signup" @click=${this.onClickLink}><span class="fas fa-fw fa-user-plus mr-1.5"></span> Sign up</a>
+            </div>
+          </div>
         </div>
-      `
-    } else {
-      return html`
-        <div class="flex flex-col">
-          <a class=${this.getMenuNavClass()} href="/" @click=${this.onClickLink}><span class="fas fa-fw fa-sign-in-alt mr-1.5"></span> Log in</a>
-          <a class=${this.getMenuNavClass()} href="/signup" @click=${this.onClickLink}><span class="fas fa-fw fa-user-plus mr-1.5"></span> <strong>Sign up</strong></a>
-        </div>
-      `
-    }
+      </header>
+    `
   }
 
   // events
